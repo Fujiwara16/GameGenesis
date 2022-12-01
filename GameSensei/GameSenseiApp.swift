29649a24f63a0e4ac28cfd9cee764b34934b9373
Nij Mehar <nijmehar16@gmail.com>
2022-12-01 16:31:37 +0530

    Initial Commit

---
+++ GameSensei/GameSenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSenseiApp.swift
+//  GameSensei
+//
+//  Created by Nij Mehar on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GameSenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
